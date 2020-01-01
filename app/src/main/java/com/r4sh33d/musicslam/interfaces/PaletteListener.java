cbfd9cd984bcacd7c5b14b7cadeca9ffca4a0b33
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2020-01-01 17:51:42 +0100

    Open source prep cleanups, add author to interfaces

--- app/src/main/java/com/r4sh33d/musicslam/interfaces/PaletteListener.java
+++ app/src/main/java/com/r4sh33d/musicslam/interfaces/PaletteListener.java
@@ -1,5 +1,7 @@
 package com.r4sh33d.musicslam.interfaces;
-
+/**
+ * @author Rasheed Sualayman (@r4sh33d)
+ */
 public interface PaletteListener {
 
     void onPaletteReady(int color);
--- app/src/main/java/com/r4sh33d/musicslam/interfaces/SlidingPanelEventsListener.java
+++ app/src/main/java/com/r4sh33d/musicslam/interfaces/SlidingPanelEventsListener.java
@@ -3,7 +3,9 @@ package com.r4sh33d.musicslam.interfaces;
 import android.view.View;
 
 import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState;
-
+/**
+ * @author Rasheed Sualayman (@r4sh33d)
+ */
 public interface SlidingPanelEventsListener {
 
     void onPanelSlide(View panel, float slideOffset);
